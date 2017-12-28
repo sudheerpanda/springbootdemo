c6d2fe535528aa28d051dcc9dba1c5f2816543bc
sudheerpanda <sudheer.panda@imaginea.com>
2017-12-28 17:57:20 +0530

    controller changes

--- src/main/java/com/imaginea/spring/SpringBootController.java
+++ src/main/java/com/imaginea/spring/SpringBootController.java
@@ -8,6 +8,7 @@ package com.imaginea.spring;
 
 import com.imaginea.exception.ServiceException;
 import com.imaginea.model.Book;
+import com.imaginea.model.BookDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -26,7 +27,8 @@ public class SpringBootController {
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(SpringBootController.class);
 
-
+	@Autowired
+	private BookDao _bookDao;
 	@ExceptionHandler(ServiceException.class)
 	@ResponseBody
 	@RequestMapping(value = "/get", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -35,8 +37,7 @@ public class SpringBootController {
 			throws ServiceException, IOException {
 		LOGGER.info("Getting the Book details by name");
 
-		Book book =new Book();
-		book.setName(name);
+		Book book =_bookDao.g
 		book.setAuthor("by imaginea");
 		LOGGER.info("Successfully got the book details by name");
 		return book;
